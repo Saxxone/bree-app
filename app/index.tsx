36296ed6f2743c5e9dbe1c26a3dd740d8e7718a1
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-11-26 13:07:25 +0000

    app setup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Bree</Text> 
+      <Text>Be Free</Text>
+      <Text>HMR IS QUITE FAST</Text>
+    </View>
+  );
+}
