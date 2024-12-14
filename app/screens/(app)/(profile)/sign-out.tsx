01f2e77f0ccd71cb8f89134fe663ff61c4fc431e
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-14 18:47:26 +0000

    fix(code): minor fix

--- app/screens/(app)/(profile)/sign-out.tsx
+++ app/screens/(app)/(profile)/sign-out.tsx
@@ -1,19 +1,20 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { useSession } from "@/ctx";
 import tailwindClasses from "@/services/ClassTransformer";
+import AppText from "@/components/app/AppText";
 
 export default function Index() {
   const { signOut } = useSession();
   return (
     <View style={tailwindClasses("container")}>
-      <Text
+      <AppText
         onPress={() => {
           // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
           signOut();
         }}
       >
         Sign Out
-      </Text>
+      </AppText>
     </View>
   );
 }
