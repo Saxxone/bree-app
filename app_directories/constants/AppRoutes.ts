483b664da82b1c93f934f0fd1316d3720ca58715
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2025-01-20 17:26:10 +0000

    folder structure changed

--- app_directories/constants/AppRoutes.ts
+++ app_directories/constants/AppRoutes.ts
@@ -1,15 +1,14 @@
 export const app_routes: Record<string, any> = {
   post: {
-    home: "/screens/(app)/(tabs)/(home)",
-    compose: "/screens/compose",
-    edit: (id: string) => `/screens/(app)/(home)/post/${id}/edit`,
-    view: (id: string) => `/screens/(app)/(tabs)/(home)/(post)/${id}`,
-    view_media: (id: string) =>
-      `/screens/(app)/(tabs)/(home)/(post)/media/${id}`,
+    home: "/(app)/(tabs)/(home)",
+    compose: "/compose",
+    edit: (id: string) => `/(app)/(home)/post/${id}/edit`,
+    view: (id: string) => `/(app)/(tabs)/(home)/(post)/${id}`,
+    view_media: (id: string) => `/(app)/(tabs)/(home)/(post)/media/${id}`,
   },
   auth: {
-    login: "/screens/(auth)/login",
-    register: "/screens/(auth)/register",
-    forgot_password: "/screens/(auth)forgot-password",
+    login: "/(auth)/login",
+    register: "/(auth)/register",
+    forgot_password: "/(auth)forgot-password",
   },
 };
