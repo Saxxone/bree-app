4fe33205fc738abbf849728ce38e829636c53776
Stephen Udoekpo <saxxone17@gmail.com>
2025-02-23 22:47:15 +0000

    minor fix

--- app_directories/services/ClassTransformer.ts
+++ app_directories/services/ClassTransformer.ts
@@ -10,19 +10,29 @@ import tailwind_to_RN_map from "./tailwind";
 
 type ClassKeys = keyof typeof tailwind_to_RN_map;
 
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K} ${P}`
+    : never
+  : never;
 
-type Join<K, P> = K extends string | number ? P extends string | number ? `${K} ${P}` : never : never;
+type Combinations<
+  T extends string,
+  U extends string = T,
+  Depth extends number = 3,
+> = Depth extends 0
+  ? never
+  : T extends any
+    ? T | Join<T, Combinations<Exclude<U, T>, Exclude<U, T>, Decrement<Depth>>>
+    : never;
 
-type Combinations<T extends string, U extends string = T, Depth extends number = 3> = 
-  Depth extends 0 ? never : T extends any ? T | Join<T, Combinations<Exclude<U, T>, Exclude<U, T>, Decrement<Depth>>> : never;
-
-type Decrement<N extends number> =  
-  N extends 1 ? 0 : never;
+type Decrement<N extends number> = N extends 1 ? 0 : never;
 
 type SpaceSeparatedKeys = Combinations<ClassKeys>;
 
-
-const tailwindClasses = (class_name: SpaceSeparatedKeys): StyleSheet.NamedStyles<object> => {
+const tailwindClasses = (
+  class_name: SpaceSeparatedKeys,
+): StyleSheet.NamedStyles<object> => {
   let styles = {};
   const classes = class_name.split(" ");
 
